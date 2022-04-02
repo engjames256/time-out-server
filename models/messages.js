adfd94d46b76aa9e8c50448914530f58472f1e78
Oyee James <engjames256@gmail.com>
2022-04-02 08:21:11 +0300

    Fixed Timestamps in the message model

--- models/messages.js
+++ models/messages.js
@@ -1,10 +1,25 @@
 import mongoose from "mongoose";
+import Inc from "mongoose-sequence";
+const AutoIncrement = Inc(mongoose);
 
-const messageSchema = new mongoose.Schema({
-  message: {
-    type: String,
-    required: [true, "Must provide Message"],
+const messageSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+    },
+    message: {
+      type: String,
+      required: [true, "Must provide Message"],
+    },
   },
+  {
+    timestamps: true,
+  }
+);
+
+messageSchema.plugin(AutoIncrement, {
+  id: "message_seq",
+  inc_field: "id",
 });
 
 const message = mongoose.model("message", messageSchema);
